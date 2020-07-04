9f76d41f440c27ab45c51664ecb0700a2a19f4d3
FilloPrinci <filloprinc98@gmail.com>
2020-07-04 17:18:06 +0200

    Lezione 8 (completa)

--- recensioni_site/forum/views.py
+++ recensioni_site/forum/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Sezione
+from .mixins import StaffMixing
 
 # Create your views here.
-class CreaSezione(CreateView):
+class CreaSezione(StaffMixing, CreateView):
     model = Sezione
     fields = "__all__"
     template_name = "forum/crea_sezione.html"
+    success_url = "/"
 
