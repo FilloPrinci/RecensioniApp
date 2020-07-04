7c88e00ed01b628b59a9ae1d5808f8e1c754130c
FilloPrinci <fipri98@gmail.com>
2020-07-04 14:54:30 +0200

    Lezione 8 (prima parte)

--- recensioni_site/forum/views.py
+++ recensioni_site/forum/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Sezione
 
 # Create your views here.
+class CreaSezione(CreateView):
+    model = Sezione
+    fields = "__all__"
+    template_name = "forum/crea_sezione.html"
+
