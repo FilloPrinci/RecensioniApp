c386b75168708a33b45347988f5c16472cb5576c
jihed <jihed.fatnassi@capgemini.com>
2020-08-19 18:14:43 +0200

    modifica e elimina annuncio

--- recensioni_site/core/urls.py
+++ recensioni_site/core/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='homepage'),
     path('users/', views.UserList.as_view(), name='user_list'),
-    path('user/<username>/', views.userProfileView, name='user_profile')
+    path('sezione/<int:pk>/modifica/',
+         login_required(views.ArticoloChange.as_view()), name="articolo-modifica"),
+    path('user/<username>/', views.userProfileView, name='user_profile'),
+    path('articolo/<int:pk>/delete/',
+         login_required(views.ArticleDelete.as_view()),  name='articolo-delete'),
 ]
--- recensioni_site/core/views.py
+++ recensioni_site/core/views.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 from forum.models import Sezione
 from django.views.generic.list import ListView
-
+from django.views.generic import DeleteView, UpdateView, DetailView, FormView
+from forum.views import visualizzaSezione
+from django.urls import reverse_lazy
 # Create your views here.
 
 class HomeView(ListView):
@@ -24,4 +26,52 @@ def userProfileView(request, username):
     return render(request, 'core/user_profile.html', context)
 
 
+class ArticoloChange(UpdateView):
+    '''
+    Modifica il titolo dell'articolo
+    '''
+    model = Sezione
+    fields = ("nome_sezione", "descrizione", "citta",
+              "provincia", "indirizzo", "logo_sezione")
+    template_name = 'core/modifica.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        user = self.request.user
+        articleid = self.kwargs['pk']
+        articolo = get_object_or_404(Sezione, id=articleid)
+
+        if user.id is not articolo.user.id:
+            return visualizzaSezione(request, articleid)
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        articleid = self.kwargs['pk']
+        articolo = get_object_or_404(Sezione, id=articleid)
+        user = get_object_or_404(User, username=articolo.user)
+        return reverse_lazy('user_profile', kwargs={'username': user})
+
+
+class ArticleDelete(DeleteView):
+    '''
+    Eliminazione di un articolo
+    '''
+    model = Sezione
+    template_name = 'core/deletearticle.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        user = self.request.user
+        articleid = self.kwargs['pk']
+        articolo = get_object_or_404(Sezione, id=articleid)
+
+        if user.id is not articolo.user.id:
+            return visualizzaSezione(request, articleid)
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        articleid = self.kwargs['pk']
+        articolo = get_object_or_404(Sezione, id=articleid)
+        user = get_object_or_404(User, username=articolo.user)
+        return reverse_lazy('user_profile', kwargs={'username': user})
 
